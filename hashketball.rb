84c27c4357c0b2290540d8fad8d37d5f56051e0a
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-20 04:17:56 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,5 +133,5 @@ def game_hash
   end
 
   def player_stats(player)
-    game_hash.map{|key,val|val[:players].map{|k,v|k.reject!{|k|k==:player_name} if k[:player_name]==player}}.flatten.compact
+    game_hash.map{|key,val|val[:players].map{|k,v|k.reject!{|k|k==:player_name} if k[:player_name]==player}}.flatten.compact[0]
   end
