2b00f7723add44f2473df82baecbc6e19217657f
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-20 03:34:07 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,5 +121,5 @@ def game_hash
   end
 
   def team_colors(team)
-    game_hash.map{|k,team_data| team_data[:colors] if team_data[:team_name] == team}.compact
+    game_hash.map{|k,team_data| team_data[:colors] if team_data[:team_name] == team}.flatten
   end
