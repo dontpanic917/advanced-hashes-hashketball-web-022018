d5bd33f292ed603ad1f6b554b389f81683597e78
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-20 03:23:32 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,7 +113,7 @@ def game_hash
   end
 
   def num_points_scored(player)
-    game_hash.map{|key,val|val[:players].map{|k,v|k[:player_name]==player ? k[:points] : return nil }.compact}.compact
+    game_hash.map{|key,val|val[:players].map{|k,v|k[:points] if k[:player_name]==player}.compact}
   end
 
   def shoe_size
